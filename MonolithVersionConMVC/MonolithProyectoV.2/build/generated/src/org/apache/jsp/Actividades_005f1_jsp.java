f1d7a361dd6fd11509226e363f4b69be75f2c7c8
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2018-02-25 12:44:15 -0600

    Actualizacion Actividades

---
+++ MonolithVersionConMVC/MonolithProyectoV.2/build/generated/src/org/apache/jsp/Actividades_005f1_jsp.java
@@ -0,0 +1,147 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Actividades_005f1_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>JSP Page</title>\n");
+      out.write("        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("        <link href=\"Css/bootstrap.css\" rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write("        <link href=\"Css/BarraDeInicioSesion.css\" rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write("        <link href=\"Css/ActividadesCSS.css\" rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write("        <link href=\"Css/InicioSesion.css\" rel=\"stylesheet\" type=\"text/css\">\n");
+      out.write("        <script src=\"js/jquery-3.2.1.min.js\"></script>\n");
+      out.write("        <script src=\"js/popper.min.js\"></script>\n");
+      out.write("        <script src=\"js/bootstrap.min.js\"></script>\n");
+      out.write("        <script src=\"js/jquery.validate.js\"></script>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <nav class=\"navbar navbar-expand-lg navbar-light  BarraDeInicio\">\n");
+      out.write("            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo03\" aria-controls=\"navbarTogglerDemo03\" aria-expanded=\"true\" aria-label=\"Toggle navigation\">\n");
+      out.write("                <span class=\"navbar-toggler-icon\"></span>\n");
+      out.write("            </button>\n");
+      out.write("            <a class=\"navbar-brand\"><img src=\"img/pawn.svg\" id=\"LogoBarraInicio\"></a>\n");
+      out.write("\n");
+      out.write("            <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo03\">\n");
+      out.write("                <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">\n");
+      out.write("                    <li class=\"nav-item\">\n");
+      out.write("                        <a class=\"nav-link\"  href=\"Actividades.jsp\"><img src=\"img/signing-the-contract.svg\" class=\"ImagenesBarraInicio\" >Actividades</a>\n");
+      out.write("                    </li>\n");
+      out.write("                    <li class=\"nav-item\">\n");
+      out.write("                        <a class=\"nav-link\"  href=\"\"><img src=\"img/group.svg\" class=\"ImagenesBarraInicio\" >Grupo</a>\n");
+      out.write("                    </li>\n");
+      out.write("                    <li class=\"nav-item\">\n");
+      out.write("                        <a class=\"nav-link\"  href=\"\"><img src=\"img/post-it.svg\" class=\"ImagenesBarraInicio\" >Notas</a>\n");
+      out.write("                    </li>\n");
+      out.write("\n");
+      out.write("                </ul>\n");
+      out.write("                <ul class=\"navbar-nav mr-left mt-2 mt-lg-0\">\n");
+      out.write("                    <li class=\"nav-item dropdown\">\n");
+      out.write("                        <a class=\"nav-link dropdown-toggle\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"true\">\n");
+      out.write("                            <img src=\"img/user.svg\" class=\"ImagenesBarraInicio\" > Usuario\n");
+      out.write("                        </a>\n");
+      out.write("                        <div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownMenuLink\" style=\"align-content:center;\">\n");
+      out.write("                            <a class=\"dropdown-item\" href=\"CerrarSesion.jsp\"><img src=\"img/enter.svg\" class=\"ImagenesBarraInicio\" > Cerrar Sesion</a>\n");
+      out.write("                            <a class=\"dropdown-item\" href=\"Configuracion.jsp\"><img src=\"img/settings-work-tool.svg\" class=\"ImagenesBarraInicio\" >\n");
+      out.write("                                Configuracion</a>\n");
+      out.write("                        </div>\n");
+      out.write("                    </li>\n");
+      out.write("                </ul>\n");
+      out.write("            </div>\n");
+      out.write("        </nav>\n");
+      out.write("        <div class=\"container-fluid\">\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("            <div class=\"row\" style=\"margin:13px;\">\n");
+      out.write("                <div class=\"col-12\">\n");
+      out.write("                    <div class=\"row\" >\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("                        <div class=\"col-lg-4 col-md-4 col-sm-12 Contenedores\" style=\"padding:12px;\">\n");
+      out.write("                            <div class=\"row\">\n");
+      out.write("                                <div class=\"col-12\">\n");
+      out.write("                                    ffdfdfd\n");
+      out.write("                                </div>\n");
+      out.write("                            </div>\n");
+      out.write("                        </div>\n");
+      out.write("\n");
+      out.write("                        <div class=\"col-lg-8 col-md-8 col-sm-12 Contenedores\">\n");
+      out.write("                            <div class=\"row\">\n");
+      out.write("                                <div class=\"col-12\">\n");
+      out.write("                                    fdfdffdf\n");
+      out.write("                                </div>\n");
+      out.write("                            </div>\n");
+      out.write("\n");
+      out.write("                        </div>\n");
+      out.write("                    </div>\n");
+      out.write("                </div>\n");
+      out.write("\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("    </div>\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("</body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
--- MonolithVersionConMVC/MonolithVersion3/MonolithV3/web/Actividades.jsp
+++ MonolithVersionConMVC/MonolithVersion3/MonolithV3/web/Actividades.jsp
@@ -6,20 +6,23 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Actividades</title>
+        <title>JSP Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="Css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="Css/BarraDeInicioSesion.css" rel="stylesheet" type="text/css">
-         <link href="Css/InicioSesion.css" rel="stylesheet" type="text/css">
+        <link href="Css/ActividadesCSS.css" rel="stylesheet" type="text/css">
+
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/popper.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.validate.js"></script>
     </head>
     <body>
-       <nav class="navbar navbar-expand-lg navbar-light  BarraDeInicio">
+        <nav class="navbar navbar-expand-lg navbar-light  BarraDeInicio">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="true" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -28,28 +31,100 @@
             <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link navbartext"  href="Actividades.jsp"><img src="img/signing-the-contract.svg" class="ImagenesBarraInicio" >Actividades</a>
-                    </li>
-                     <li class="nav-item">
-                         <a class="nav-link navbartext"  href=""><img src="img/group.svg" class="ImagenesBarraInicio" >Grupos</a>
+                        <a class="nav-link"  href="Actividades.jsp"><img src="img/signing-the-contract.svg" class="ImagenesBarraInicio" >Actividades</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link navbartext"  href=""><img src="img/post-it.svg" class="ImagenesBarraInicio" >Notas</a>
+                        <a class="nav-link"  href=""><img src="img/group.svg" class="ImagenesBarraInicio" >Grupo</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link"  href=""><img src="img/post-it.svg" class="ImagenesBarraInicio" >Notas</a>
+                    </li>
+
                 </ul>
                 <ul class="navbar-nav mr-left mt-2 mt-lg-0">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle navbartext" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             <img src="img/user.svg" class="ImagenesBarraInicio" > Usuario
                         </a>
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink" style="align-content:center;">
-                            <a class="dropdown-item navbartext-pop" href="CerrarSesion.jsp"><img src="img/enter.svg" class="ImagenesBarraInicio" > Cerrar Sesion</a>
-                            <a class="dropdown-item  navbartext-pop" href="Configuracion.jsp"><img src="img/settings-work-tool.svg" class="ImagenesBarraInicio" >
+                            <a class="dropdown-item" href="CerrarSesion.jsp"><img src="img/enter.svg" class="ImagenesBarraInicio" > Cerrar Sesion</a>
+                            <a class="dropdown-item" href="Configuracion.jsp"><img src="img/settings-work-tool.svg" class="ImagenesBarraInicio" >
                                 Configuracion</a>
                         </div>
                     </li>
                 </ul>
             </div>
         </nav>
+        <div class="container-fluid">
+            <div class="row" >
+                <div class="col-lg-8 col-md-8 col-sm-12">
+                    <div class="row BandejaActividades">
+                        <div class="col-12 ">
+                            <div class="row">
+                                <div class="col-12 TituloContenedores">
+                                    <div class="row">
+                                        <div class="col-8" style="padding-top:40px;">
+                                            <a>Bandeja Principal de Actividades</a>
+                                        </div> 
+                                        <div class="col-4">
+                                             <img class="float-right" src="img/copy.svg" >
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 SeparadorTituloContendores">
+             
+                                </div>
+                            </div>
+                            <div class="row" style="overflow-y:scroll;height:330px;" >
+                                <div class="col-12" >
+                                    <div class="row ActividadDiv" style="margin:2px;height:20px;background-color:#0062cc;">
+                                        <div class="col-12">
+                                            <div class="row">
+                                                <div class="col-3">
+                                                    Actividad
+                                                </div>
+                                                 <div class="col-3">
+                                                     Fecha
+                                                </div>
+                                                 <div class="col-3">
+                                                     Localizacion
+                                                </div>
+                                                 <div class="col-3">
+                                                     Categoria
+                                                </div>
+                                            </div> 
+                                            <div class="row">
+                                                <div class="col-3">
+                                                    <input type="text" placeholrer="Nombre">
+                                                </div>
+                                                 <div class="col-3">
+                                                     <input type="text" placeholrer="Fecha">
+                                                </div>
+                                                 <div class="col-3">
+                                                    <input type="text" placeholrer="Categoria">
+                                                </div>
+                                                 <div class="col-3">
+                                                     Categoria
+                                                </div>
+                                            </div> 
+                                        </div>
+                                    </div>   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12" >
+                    <div class="row BandejaActividades" >
+                        <div class="col-12 ">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
